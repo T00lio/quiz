f04efb430f9465f71adb9d85ba2438e55487b458
tuliosalvatierra <salvacorp@gmail.com>
2024-02-05 18:32:01 -0600

    Refactor Home page layout and add sign-in functionality

--- client/quiz/src/pages/Home.jsx
+++ client/quiz/src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import styles from "./Home.css";
+
 const Home = () => {
   const google = () => {
     window.open("http://localhost:3004/auth/google", "_self");
@@ -11,21 +14,55 @@ const Home = () => {
   };
 
   return (
-    <div className="result" style={{ marginTop: "10rem" }}>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the content of the home page.</p>
-      <button type="button" onClick={google}>
-        {" "}
-        Google
-      </button>
-      <button type="button" onClick={github}>
-        {" "}
-        Github
-      </button>
-      <button type="button" onClick={facebook}>
-        {" "}
-        Facebook
-      </button>
+    <div className="result">
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Welcome Back!</h1>
+          <p className={styles.subtitle}>Sign in to continue to your account</p>
+        </div>
+        <div>
+          <div className={styles.form}>
+            <div className={styles.formGroup}>
+              <label className={styles.label} htmlFor="username">
+                Username
+              </label>
+              <input className={styles.input} id="username" required />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.label} htmlFor="password">
+                Password
+              </label>
+              <input
+                className={styles.input}
+                id="password"
+                required
+                type="password"
+              />
+            </div>
+            <button className={styles.submitButton} type="submit">
+              Sign In
+            </button>
+          </div>
+          <separator className={styles.separator} />
+          <div className={styles.socialButtons}>
+            <button className={styles.googleButton} onClick={google}>
+              Sign in with Google
+            </button>
+            <button className={styles.facebookButton} onClick={facebook}>
+              Sign in with Facebook
+            </button>
+            <button className={styles.twitterButton} onClick={github}>
+              Sign in with Github
+            </button>
+            <div className={styles.signupLink}>
+              Don't have an account?
+              <Link className={styles.signupText} href="#">
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
