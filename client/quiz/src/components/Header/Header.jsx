782589dde4393a684260afa8395b3178e6da8a17
tuliosalvatierra <salvacorp@gmail.com>
2024-02-17 14:04:53 -0600

    Add API configuration and update dependencies

--- client/quiz/src/components/Header/Header.jsx
+++ client/quiz/src/components/Header/Header.jsx
@@ -1,20 +1,87 @@
-import logo from "/react.svg";
+// import logo from "/react.svg";
 import "./Header.css";
 
 function Header() {
   return (
-    <header className="container">
-      <div className="col">
-        <h1>
-          100 React Questions{" "}
-          <img className="App-logo" src={logo} alt="react logo"></img>
-        </h1>
-
-        <p className="header-text">
-          This app will help you memorize the top 100 react interview questions
-        </p>
+    <nav className="relative pb-20 bg-black">
+      <div className="flex py-8 px-4 lg:px-8 justify-between items-center bg-transparent sticky rounded-2xl">
+        <a className="text-2xl text-white font-bold" href="#">
+          <img
+            className="h-7"
+            src="zospace-assets/logos/zospace-logo.svg"
+            alt=""
+            width="auto"
+          />
+        </a>
+        <div className="lg:hidden">
+          <button className="p-2 navbar-burger">
+            <svg
+              className="w-10 h-3"
+              width={39}
+              height={13}
+              viewBox="0 0 39 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width={39} height={2} rx={1} fill="#C4C4C4" />
+              <rect x={19} y={11} width={20} height={2} rx={1} fill="#C4C4C4" />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:block absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+          <ul className="flex items-center text-white space-x-10 text-center">
+            <li>
+              <span></span>
+            </li>
+            <li>
+              <a className="text-white font-bold text-lg" href="#">
+                Quizes
+              </a>
+            </li>
+            <span>
+              <svg
+                width={5}
+                height={5}
+                viewBox="0 0 5 5"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="2.5" cy="2.5" r="2.5" fill="#726B6B" />
+              </svg>
+            </span>
+            <li>
+              <a className="text-white font-bold text-lg" href="#">
+                About
+              </a>
+            </li>
+            <span>
+              <svg
+                width={5}
+                height={5}
+                viewBox="0 0 5 5"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="2.5" cy="2.5" r="2.5" fill="#726B6B" />
+              </svg>
+            </span>
+            <li>
+              <a className="text-white font-bold text-lg" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden lg:block">
+          <a
+            className="inline-block px-12 py-4 text-white font-bold border border-gray-200 hover:border-white rounded-2xl"
+            href="#"
+          >
+            Start here
+          </a>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 }
 
---
+++ client/quiz/src/pages/quizPage.jsx
@@ -0,0 +1,164 @@
+import { useState, useEffect } from "react";
+import api from "../api/api";
+
+import logo from "/react.svg";
+
+function QuizPage() {
+  const [number, setNumber] = useState(0);
+  const [questions, setQuestions] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [restultBG, setResultBG] = useState("");
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/api/questions");
+        setQuestions(response.data.data);
+        console.log(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleClicked = (correct1, correct2, correct3, correct4) => {
+    let isCorrect = false;
+    if (correct1 === "TRUE") {
+      isCorrect = true;
+    }
+    if (correct2 === "TRUE") {
+      isCorrect = true;
+    }
+    if (correct3 === "TRUE") {
+      isCorrect = true;
+    }
+    if (correct4 === "TRUE") {
+      isCorrect = true;
+    }
+    if (isCorrect) {
+      setScore(score + 1);
+    } else {
+      setScore(score);
+    }
+
+    const newBackgroundColor = isCorrect ? "green" : "red";
+    setResultBG(newBackgroundColor);
+    setTimeout(() => {
+      setResultBG("");
+      const nextQuest = number + 1;
+      if (nextQuest < questions.length) {
+        setNumber(nextQuest);
+      } else {
+        setShowResult(true);
+      }
+    }, 1500);
+  };
+
+  const handleRestart = () => {
+    setNumber(0);
+    setScore(0);
+    setShowResult(false);
+  };
+
+  const questionData = questions[number];
+
+  return (
+    <>
+      <div className="App">
+        <header className="Header">
+          <h1
+            style={{
+              textAlign: "center",
+              alignItems: "center",
+              fontWeight: "900",
+              justifyContent: "center",
+              display: "flex",
+              fontSize: "4rem",
+            }}
+          >
+            100 React Question{" "}
+            <img
+              className="App-logo"
+              src={logo}
+              alt="react logo"
+              style={{ width: "40px", height: "40px" }}
+            ></img>
+          </h1>
+          <p
+            style={{
+              textAlign: "center",
+              fontSize: "1.5rem",
+              fontWeight: "500",
+            }}
+          >
+            This app will help you memorize the top 100 react interview
+            questions
+          </p>{" "}
+        </header>
+
+        <div>
+          <h2 style={{ textAlign: "center" }}>Result</h2>
+          <strong>
+            You scored {score} out of {questions.length}
+          </strong>
+        </div>
+
+        <section className="Question">
+          {" "}
+          <h1>{questionData?.question}</h1>
+        </section>
+        <div className="Result" style={{ backgroundColor: restultBG }}>
+          {/* <h1>good job</h1> */}
+        </div>
+        <section className="Options">
+          <button
+            className="OptionButton"
+            onClick={() => handleClicked(questionData?.correct1)}
+          >
+            {questionData?.option1}
+          </button>
+          <button
+            className="OptionButton"
+            onClick={() => handleClicked(questionData?.correct2)}
+          >
+            {questionData?.option2}
+          </button>
+          <button
+            className="OptionButton"
+            onClick={() => handleClicked(questionData?.correct3)}
+          >
+            {questionData?.option3}
+          </button>
+          <button
+            className="OptionButton"
+            onClick={() => handleClicked(questionData?.correct4)}
+          >
+            {questionData?.option4}
+          </button>
+        </section>
+        <footer className="Footer">
+          <div className="row">
+            <div className="col">
+              <h5 className="dashboard">
+                Question: {number} of {questions.length}
+              </h5>
+              <h5 className="dashboard">Time: 10000</h5>
+              <h5 className="dashboard">user</h5>
+              Score: {score} out of: {questions?.length}
+            </div>
+            <h5 className="col" style={{ textAlign: "center" }}>
+              <button className="button" onClick={handleRestart}>
+                Click here to restart
+              </button>
+
+              {showResult}
+            </h5>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
+export default QuizPage;
