1982f03a2f9f5ca69722bbb5632fb7eb54cc5994
tuliosalvatierra <salvacorp@gmail.com>
2024-03-17 23:19:06 -0500

    Add logout functionality to GoogleButton component

--- client/src/components/GoogleButton/GoogleButton.jsx
+++ client/src/components/GoogleButton/GoogleButton.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function GoogleButton() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
   return (
     <div>
-      <button
-        onClick={() => loginWithRedirect()}
-        className="mb-3 py-4 w-full bg-orange-500 hover:bg-orange-600 text-white font-bold rounded-full transition duration-200"
-      >
-        Google
-      </button>
+      {isAuthenticated ? (
+        <button
+          onClick={() => logout()}
+          className="mb-3 py-4 w-full bg-orange-500 hover:bg-orange-600 text-white font-bold rounded-full transition duration-200"
+        >
+          Logout
+        </button>
+      ) : (
+        <>
+          <button
+            onClick={() => loginWithRedirect()}
+            className="mb-3 py-4 w-full bg-orange-500 hover:bg-orange-600 text-white font-bold rounded-full transition duration-200"
+          >
+            Google
+          </button>
+        </>
+      )}
     </div>
   );
 }
