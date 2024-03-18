e5e8f5cccd6f4904b61815111726e009f27d1dc6
tuliosalvatierra <salvacorp@gmail.com>
2024-03-17 23:33:56 -0500

    Refactor Profile component to simplify conditional rendering

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -3,25 +3,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "../GoogleLogoutButton/LogoutButton";
 
 function Profile() {
-  const { user, isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
   return (
     <div>
-      {isAuthenticated ? (
-        <>
-          <h1>Profile</h1>
-          <h1>{user.name}</h1>
-          <img src={user.picture} alt={user.name} />
-          <p>{user.email}</p>
-          <LogoutButton />
+      <>
+        <h1>Profile</h1>
+        <h1>{user.name}</h1>
+        <img src={user.picture} alt={user.name} />
+        <p>{user.email}</p>
+        <LogoutButton />
 
-          <div>{JSON.stringify(user, null, 2)}</div>
-        </>
-      ) : (
-        <>
-          <h1>Profile</h1>
-          <h1>Not logged in</h1>
-        </>
-      )}
+        <div>{JSON.stringify(user, null, 2)}</div>
+      </>
     </div>
   );
 }
