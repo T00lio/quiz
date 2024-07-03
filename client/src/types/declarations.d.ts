ab8235d31cdb458329b2851d9ac42dabd8b237ff
tuliosalvatierra <salvacorp@gmail.com>
2024-07-02 22:24:16 -0500

    refactor: Update tsconfig.json to include path aliases for components and pages

---
+++ client/src/types/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module "./pages/QuizMenu" {
+  const QuizMenu: React.ComponentType;
+  export default QuizMenu;
+}
+
+declare module "./pages/QuizPage_new" {
+  const QuizPage_new: React.ComponentType;
+  export default QuizPage_new;
+}
